33bcb5db239320758deb02916cd50b1d1a5089b6
Cuneyt Mertayak <cmertayak@gmail.com>
2014-08-11 00:05:11 -0700

    Adding Concatination Logic for baseUrl already having request param
    For the base urls having request param page param is added with “&”
    instead of “?”

--- test/render.js
+++ test/render.js
@@ -26,6 +26,14 @@ describe('Paginate', function() {
 			done();
 		});
 
+        it('should return the correct html per the default templates for baseUrl already containing URL parameters', function(done) {
+            var paging = paginate.page(data.length, PER_PAGE, 1);
+            var html = paging.render({ baseUrl: '/example?a=1&b=2' });
+            html.should.eql('<div class="pagination"><ul><li><a class="active">1</a></li><li><a href="/example?a=1&amp;b=2&amp;page=2">2</a></li><li><a href="/example?a=1&amp;b=2&amp;page=3">3</a></li><li><span class="separator">...</span></li><li><a href="/example?a=1&amp;b=2&amp;page=10">10</a></li><li><a href="/example?a=1&amp;b=2&amp;page=2">Next &rsaquo;</a></li></ul></div>');
+
+            done();
+        });
+
 	});
 
 });
